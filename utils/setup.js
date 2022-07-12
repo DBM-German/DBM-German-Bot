ab17d0a37dd41336c932b28626ca4468e2216d96
ACertainCoder <47316898+ACertainCoder@users.noreply.github.com>
2022-07-12 21:37:00 +0200

    Update setup script
    The setup now checks all Steam library folders. This solves issue #2

--- utils/setup.js
+++ utils/setup.js
@@ -1,7 +1,8 @@
 import { exec as execCb } from "child_process";
 import { constants } from "fs";
-import { access, cp, rm } from "fs/promises";
+import { access, cp, readFile, rm } from "fs/promises";
 import { promisify } from "util";
+import * as VDF from "vdf-parser";
 
 const exec = promisify(execCb);
 
@@ -10,10 +11,31 @@ const RAW_DIR = "./raw";
 const RAW_DATA_DIR = `${RAW_DIR}/data`;
 const BOT_DIR = "./bot";
 const BOT_DATA_DIR = `${BOT_DIR}/data`;
-const DBM_DIR = "C:/Program Files (x86)/Steam/steamapps/common/Discord Bot Maker/";
-const DBM_TEMPLATE_DIR = `${DBM_DIR}/resources/app/bot`;
-const DBM_ACTIONS_DIR = `${DBM_DIR}/actions`;
+const BOT_ACTIONS_DIR = `${BOT_DIR}/actions`;
+const STEAM_DIR = "C:/Program Files (x86)/Steam";
+const STEAM_LIB_CONFIGS = [
+    `${STEAM_DIR}/config/libraryfolders.vdf`,
+    `${STEAM_DIR}/steamapps/libraryfolders.vdf`
+];
+const DBM_ID = "682130";
+const DBM_TEMPLATE_FOLDER = "resources/app/bot";
+const DBM_ACTIONS_FOLDER = "actions";
 
+
+/**
+ * @typedef SteamLibApps
+ * @type {{ [appid: string]: string }}
+ */
+
+/**
+ * @typedef SteamLibFolder
+ * @type {{ path: string; label: string; contentid: number; totalsize: number; update_clean_bytes_tally: number; time_last_update_corruption: number; apps: SteamLibApps }}
+ */
+
+/**
+ * @typedef SteamLibFolders
+ * @type {{ [index: string]: SteamLibFolder }}
+ */
 
 /**
  * File encoding
@@ -25,24 +47,72 @@ const ENCODING = "utf8";
 // Start
 console.log("Richte DBM-Projekt ein...");
 
-// Check permissions for DBM template directory
+// Check permissions for Steam directory
 try {
-    await access(DBM_TEMPLATE_DIR, constants.F_OK | constants.R_OK);
+    await access(STEAM_DIR, constants.F_OK | constants.R_OK);
 } catch(e) {
-    console.log(`Auf Discord Bot Maker-Template-Verzeichnis kann nicht zugegriffen werden: ${e}`);
+    console.log(`Auf Steam-Verzeichnis kann nicht zugegriffen werden: ${e}`);
     process.exit(1);
 }
 
-// Check permissions for DBM actions directory
+// Find library folders config and extract library folders
+
+/**
+ * Steam library folders
+ * @type SteamLibFolders
+ */
+let libraryfolders;
+let errors = [];
+
+for(let config of STEAM_LIB_CONFIGS) {
+    try {
+        await access(config, constants.F_OK | constants.R_OK);
+        ({ libraryfolders } = VDF.parse(await readFile(config, { encoding: ENCODING })));
+        break;
+    } catch(e) {
+        errors.push(e);
+    }
+}
+
+if(errors.length > 0 && !libraryfolders) {
+    console.log(`Auf Steam Bibliotheken-Konfiguration kann nicht zugegriffen werden: ${errors.map(e => e?.toString())}`);
+    process.exit(1);
+}
+
+// Find Steam library in which DBM is installed
+let dbmDir;
+
+for(let library of Object.values(libraryfolders)) {
+    if(Object.keys(library.apps).includes(DBM_ID)) {
+        dbmDir = `${library.path.replace(/\\\\/g, "/")}/steamapps/common/Discord Bot Maker`;
+        break;
+    }
+}
+
+if(!dbmDir) {
+    console.log("Discord Bot Maker-Verzeichnis kann nicht gefunden werden. Eventuell muss Steam neu gestartet werden.");
+    process.exit(1);
+}
+
+// Check permissions for DBM template folder
 try {
-    await access(DBM_ACTIONS_DIR, constants.F_OK | constants.R_OK);
+    await access(`${dbmDir}/${DBM_TEMPLATE_FOLDER}`, constants.F_OK | constants.R_OK);
 } catch(e) {
-    console.log(`Auf Discord Bot Maker-Actions-Verzeichnis kann nicht zugegriffen werden: ${e}`);
+    console.log(`Auf Discord Bot Maker-Template-Ordner kann nicht zugegriffen werden: ${e}`);
+    process.exit(1);
+}
+
+// Check permissions for DBM actions folder
+try {
+    await access(`${dbmDir}/${DBM_ACTIONS_FOLDER}`, constants.F_OK | constants.R_OK);
+} catch(e) {
+    console.log(`Auf Discord Bot Maker-Actions-Ordner kann nicht zugegriffen werden: ${e}`);
     process.exit(1);
 }
 
 // Check permissions for bot directory
 let botDirExists;
+
 try {
     await access(BOT_DIR, constants.F_OK | constants.R_OK | constants.W_OK);
     botDirExists = true;
@@ -71,8 +141,8 @@ if(botDirExists) {
 // Copy template and actions from DBM installation directory
 try {
     console.log("Kopiere DBM-Template und -Actions...");
-    await cp(DBM_TEMPLATE_DIR, BOT_DIR, { recursive: true });
-    await cp(DBM_ACTIONS_DIR, `${BOT_DIR}/actions`, { recursive: true });
+    await cp(`${dbmDir}/${DBM_TEMPLATE_FOLDER}`, BOT_DIR, { recursive: true });
+    await cp(`${dbmDir}/${DBM_ACTIONS_FOLDER}`, BOT_ACTIONS_DIR, { recursive: true });
 } catch (e) {
     console.log(`Template oder Actions können nicht kopiert werden: ${e}`);
     process.exit(1);
